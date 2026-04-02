695151b075cd24ce415603295e97bf3e72746b2a
p-olguin <p.olguinhernandez@uandresbello.edu>
2026-04-02 11:06:18 -0300

    agrega validaciones, eliminacion y persistencia en json

--- src/main.py
+++ src/main.py
@@ -1,29 +1,47 @@
-from productos import agregar_producto, mostrar_productos, total_productos
-
+from productos import (
+    agregar_producto, 
+    mostrar_productos, 
+    total_productos, 
+    eliminar_producto,
+    cargar_productos
+)
 def menu():
     while True:
         print("\n----MENÚ----")
         print("1. Agregar producto")
         print("2. Ver producto")
         print("3. Ver total")
-        print("4. Salir")
+        print("4. Eliminar producto")
+        print("5. Salir")
 
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
             nombre = input("Nombre del producto: ")
-            precio = float(input("Precio: "))
-            agregar_producto(nombre, precio)
+
+            try: #Manejo de excepciones
+                precio = float(input("Precio: "))
+                agregar_producto(nombre, precio)
+            except ValueError: #Alternativa por si falla
+                print("Debes ingresar un número válido para el precio")
 
         elif opcion == "2":
             mostrar_productos()
         
         elif opcion == "3":
             print(f"Total: ${total_productos()}")
-
-        elif opcion == "4":
-            print ("Chao")
         
+        elif opcion == "4":
+            mostrar_productos()
+            try:
+                indice = int(input("Número del producto a eliminar: ")) -1 #Indices comienzan en 0
+                eliminar_producto(indice)
+
+            except ValueError:
+                print("Debes ingresar un número válido") 
+
+        elif opcion == "5":
+            print ("Chao")
             break
 
         else:
--- src/productos.py
+++ src/productos.py
@@ -1,15 +1,45 @@
+import json #Formato de texto ligero
+
 productos = []
 
+ARCHIVO = "productos.json"
+
+def cargar_productos():
+    global productos
+    try:
+        with open(ARCHIVO, "r", encoding="utf-8") as archivo:
+            productos = json.load(archivo)
+    except FileNotFoundError:
+        producto = []
+    except json.JSONDecodeError:
+        producto = []
+
+def guardar_productos():
+    with open(ARCHIVO, "w", encoding="utf-8") as archivo:
+        json.dump(productos, archivo, indent=4, ensure_ascii=False)
+
 def agregar_producto(nombre, precio):
+    if not nombre.strip(): #Verifica si campo esta vacio
+        print("El nombre no puede estar vacio")
+        return
+    if precio <= 0:
+        print("El precio debe ser mayor que 0")
+
     producto = {
         "nombre": nombre,
         "precio": precio
     }
     productos.append(producto)
+    guardar_productos()
+    print("Producto agregado correctamente")
 
 def mostrar_productos():
-    for p in productos():
-        print(f"{p['nombre']} - ${p['precio']}")
+    if not productos:
+        print("No hay productos registrados")
+        return
+    
+    for i, p in enumerate(productos, start=1):
+        print(f"{i}. {p['nombre']} - ${p['precio']}")
 
 def total_productos():
     total = 0
@@ -17,6 +47,20 @@ def total_productos():
         total += p["precio"]
 
     return total
+
+def eliminar_producto(indice):
+    if indice <= 0 or indice >= len(productos):
+        print("Índice inválido")
+        return
+    
+    eliminado = productos.pop(indice)
+    guardar_productos()
+    print(f"Producto eliminado: {eliminado['nombre']}")
+    
+
+def buscar_producto():
+    productos.sort("nombre")
+
 
 
 
